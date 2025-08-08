d8704135d1e6e39917effabdaedf5000d162fd17
sixro <sixro@users.noreply.github.com>
2025-08-09 00:33:27 +0200

    improved code by using #define(s)

--- 1.2/celsius_fahrenheit.c
+++ 1.2/celsius_fahrenheit.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#define FARH_OFFSET 32
+#define FARH_CONVERSION_FACTOR ((float) 9) / 5
+
 int main(void) {
 	float fahr, celsius;
 	int lower, upper, step;
-	float conversion_factor;
 
 	lower = -20;
 	upper = 150;
@@ -13,9 +15,8 @@ int main(void) {
 	printf("Celsius   Fahrenheit\n");
 
 	celsius = lower;
-	conversion_factor = ((float) 9) / 5;
 	while (celsius < upper) {
-		fahr = celsius * conversion_factor + 32;
+		fahr = celsius * FARH_CONVERSION_FACTOR + FARH_OFFSET;
 		printf("%3.0f       %3.0f\n", celsius, fahr);
 
 		celsius += step;
