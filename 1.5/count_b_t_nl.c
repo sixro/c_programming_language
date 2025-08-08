625cb086583236d1c5641dde0eac6c4cc4fcb13c
sixro <sixro@users.noreply.github.com>
2025-08-09 00:57:56 +0200

    added exercise 1.8 to count blanks, tabs, newlines

---
+++ 1.5/count_b_t_nl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int c, blanks, tabs, nl;
+
+	blanks = 0;
+	tabs = 0;
+	nl = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')
+			++blanks;
+		else if (c == '\t')
+			++tabs;
+		else if (c == '\n')
+			++nl;
+	}
+
+	printf("Blanks: %d, Tabs: %d, Newlines: %d\n", blanks, tabs, nl);
+}
