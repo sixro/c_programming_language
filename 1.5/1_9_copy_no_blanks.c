2daaebae855e2a699e0aa0f5ef758b7c1c8048db
sixro <sixro@users.noreply.github.com>
2025-08-09 12:08:36 +0200

    changed code to use #define and make it more readable

--- 1.5/1_9_copy_no_blanks.c
+++ 1.5/1_9_copy_no_blanks.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int main(void) {
 	int c, printed_blank;
 
-	printed_blank = 0;
+	printed_blank = FALSE;
 
 	while ((c = getchar()) != EOF) {
 		if (c == ' ') {
 			if (! printed_blank) {
 				printf(" ");
-				printed_blank = 1;
+				printed_blank = TRUE;
 			}
 		}
 		else {
-			printed_blank = 0;
+			printed_blank = FALSE;
 			printf("%c", c);
 		}
 
