794711b4bb4476d00b635a54e9526c749bff2a3f
sixro <sixro@users.noreply.github.com>
2025-08-09 13:41:06 +0200

    added 1.15

---
+++ 1/1_15_fahr_cels_with_func.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * Returns celsius equivalent of specified temperature expressed in Fahrenheit
+ */
+float to_celsius(float fhar);
+
+int main(void) {
+	float fahr,celsius;
+	float lower,upper,step;
+
+	lower = 0;/* lower limit of temperatuire scale */
+	upper = 300;/* upper limit */
+	step = 20;/* step size */
+	fahr = lower;
+	
+	printf("Farh  Celsius\n");
+	
+	while (fahr <= upper) {
+		celsius = to_celsius(fahr);
+		printf("%3.0f %6.1f\n",fahr,celsius);
+		fahr = fahr + step;
+	}
+}
+
+float to_celsius(float fahr) {
+	return (5.0/9.0) * (fahr-32.0);
+}
