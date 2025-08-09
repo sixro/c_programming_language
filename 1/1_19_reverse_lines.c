394a760a549a5fa7a1152700bd74bbbef9424ab1
sixro <sixro@users.noreply.github.com>
2025-08-09 20:33:51 +0200

    added 1.19

---
+++ 1/1_19_reverse_lines.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+#define IN  1
+#define OUT 0
+
+int getline_internal(char line[], int maxline);
+
+void copy(char to[], char from[]);
+
+void print_reverse(char line[], int len);
+
+/*
+ * WARNING: I am not completely sure the exercise is correct, because it saying
+ * that I have to remove traling spaces and tabs, but I consider both in the
+ * algo to skip them... so, maybe it's wrong
+ */
+int main(void) {
+	int len;
+	char line[MAXLINE];
+
+	while ((len = getline_internal(line, MAXLINE)) > 0) {
+		if (len > 1)
+			print_reverse(line, len);
+	}
+	return 0;
+}
+
+void print_reverse(char line[], int len) {
+	len -= 2;
+	while (len >= 0) {
+		printf("%c", line[len--]);
+	}
+	printf("\n");
+}
+
+int getline_internal(char s[], int lim) {
+	int c, i;
+	int state;
+
+	state = OUT;
+	for (i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		if (c == ' ' || c == '\t') {
+			if (state == IN) {
+				s[i] = c;
+			} else {
+				/* go back with the counter to remove the traling characters */
+				i--;
+			}
+			state = OUT;
+		}
+		else {
+			if (state == OUT) {
+				state = IN;
+			}
+			s[i] = c;
+		}	
+	}
+	if (c == '\n') {
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[]) {
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
